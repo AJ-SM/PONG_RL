cc787ff6951facfc4dcaf2a34cba8af2eecd2734
Anuj <154649323+AJ-SM@users.noreply.github.com>
2025-08-27 15:11:34 +0530

    Update test.py

--- RL/test.py
+++ RL/test.py
@@ -13,19 +13,32 @@ env = PongSand()
 
 
 # Initialize the model
-model = PPO("MlpPolicy", env, verbose=1, tensorboard_log=logdir)
+
 
 # Training parameters
 TIMESTEPS = 10000
 steps = 300
+model = PPO.load("E:\\Storage\\Codes\\PYTHON\\PONG\\game2\\MODELS\\PPO\\1000000.zip")
 
-# Training loop
-for step in range(1, steps + 1):
-    model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps=False, tb_log_name="PPO")
-    model.save(f"{models_dir}/{TIMESTEPS * step}")
+obs = self.reset()  # Reset the environment once at the beginning
+done = False
 
-    # Run the environment to visualize the current model
-    if step % 10 == 0: 
-        print(f"Running the environment after step {step}")
+while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.close()
+                return  # Exit the game loop
 
+            # Predict the action using the trained model
+        action, _ = model.predict(obs)
+
+            # Take a step in the environment
+        obs, reward, done, _ = self.step(action)
+
+            # Render the environment
+        self.render()
+
+            # Reset the environment if done
+        if done:
+            obs = self.reset()
 env.close()
